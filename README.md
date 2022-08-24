d58d077aff64876b80eb65784fc8c3db83c6f98d
python640 <python640@gmail.com>
2022-08-24 17:27:29 +0300

    init

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# gcp_secret_puller
-Script to pull secrets from GCP secret manager
+This package could be ran from the command line in order to fetch all the secrets from the specified GCP project secret manager and store it in .env file in key/value format or in json file in json format.
+
+Prerequisites:
+
+1. Your project should have secret manager API enabled
+2. You should be logged in to GCP project 
+3. You should have sufficient permissions to read secrets in GCP secrets manager
+
+For some details please refer to following links:
+ 
+https://cloud.google.com/secret-manager/docs/reference/libraries#client-libraries-install-python
+https://cloud.google.com/python/docs/reference/secretmanager/latest
+https://cloud.google.com/secret-manager/docs/samples
+https://cloud.google.com/secret-manager/docs/filtering#examples
+
+Install requirements:
+```
+pip install -r requirements.txt
+```
+
+How to run:
+```
+python3 get_all_secrets.py [-h] [--format] [--filename] <gcp_project_id>
+
+positional arguments:
+  gcp_project_id  ID or name of the GCP project - required
+
+optional arguments:
+  -h, --help      show this help message and exit
+  --filter        Filter secrets with labels
+  --format        Output file format (json / .env). Default is .env
+  --filename      Output filename. Default is .env or .env.json (depends on --format flag)
+
+Examples:
+
+To dump all the secrets from <gcp_project_id> to .env file
+
+    python3 get_all_secrets.py <gcp_project_id>
+  
+To dump all the secrets from <gcp_project_id> filtered by label (for example with label 'env: stage')
+
+    python3 get_all_secrets.py <gcp_project_id> --filter labels.env=stage
+
+To dump all the secrets from <gcp_project_id> to .env.json file
+
+    python3 get_all_secrets.py --format json <gcp_project_id>
+
+To dump all the secrets from <gcp_project_id> to cookiemonster.env file in key/value
+
+    python3 get_all_secrets.py --filename cookiemonster.env <gcp_project_id>
+
+To dump all the secrets from <gcp_project_id> to cypress.env.json file in json format
+
+    python3 get_all_secrets.py --format json --filename cypress.env <gcp_project_id>
+```
---
+++ get_all_secrets.py
@@ -0,0 +1,140 @@
+
+import re
+import json
+import argparse
+import os, sys; sys.path.insert(0, os.path.dirname(__file__))
+from google.cloud import secretmanager
+
+def main():
+    example_text = '''
+    Examples:
+
+    To dump all the secrets from <gcp_project_id> to .env file
+
+        python3 get_all_secrets.py <gcp_project_id>
+
+
+    To dump all the secrets from <gcp_project_id> filtered by label (for example with label 'env: stage')
+
+        python3 get_all_secrets.py <gcp_project_id> --filter labels.env=stage
+
+
+    To dump all the secrets from <gcp_project_id> to .env.json file
+
+        python3 get_all_secrets.py --format json <gcp_project_id>
+
+
+    To dump all the secrets from <gcp_project_id> to cookiemonster.env file in key/value
+
+        python3 get_all_secrets.py --filename cookiemonster.env <gcp_project_id>
+
+
+    To dump all the secrets from <gcp_project_id> to cypress.env.json file in json format
+
+        python3 get_all_secrets.py --format json --filename cypress.env <gcp_project_id>
+    '''
+    parser = argparse.ArgumentParser(
+      description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter, epilog=example_text, prog='python3 get_all_secrets.py', 
+      usage='gcp_secrets.get_all_secrets [-h] [--format] [--filename] <gcp_project_id>'
+    )
+    parser.add_argument("gcp_project_id", type = str, help = "ID or name of the GCP project - required")
+    parser.add_argument("--filter", type = str, help = "Filter secrets with labels", metavar = '' ,required = False)    
+    parser.add_argument("--format", type = str, choices=['json', '.env'], help = "Output file format (json / .env). Default is .env", metavar = '' ,required = False, default = ".env")
+    parser.add_argument("--filename", type = str, help = "Output filename. Default is .env or .env.json (depends on --format flag)", metavar = '' ,required = False, default = ".env")
+
+    args = parser.parse_args()
+    # Create the secret manager client
+    client = secretmanager.SecretManagerServiceClient()
+
+    gcp_project_id = args.gcp_project_id
+    filter_str = args.filter
+    filename = args.filename
+    secrets = list_secrets_with_filter(gcp_project_id, filter_str, client)
+    secrets_ver = access_secret_version(gcp_project_id, secrets, client)
+    # Get all the project secrets to .env file (JSON or key/value text)
+    if (args.format=="json"):
+        secrets_to_json(secrets_ver, gcp_project_id, filename)
+    else:
+        secrets_to_env(secrets_ver, gcp_project_id, filename)
+
+def list_secrets_with_filter(gcp_project_id, filter_str, client):
+
+    # Build the resource name of the parent project
+    parent = f"projects/{gcp_project_id}"
+    
+    secrets = {}
+
+    # List all secrets names with filter
+    if not filter_str:
+        print("\nListing all the secrets names in project "+gcp_project_id)
+    else:
+        print("\nListing all the secrets names in project "+gcp_project_id+" with filter "+filter_str)
+    try:
+        for secret in client.list_secrets(request={"parent": parent, "filter": filter_str}):
+            # print(client.list_secrets(request={"parent": parent, "filter": filter_str}))
+            strValue = re.sub(".*/", '', format(secret.name))
+            secrets[strValue] = None
+            print("Found secret "+strValue)    
+            # print(secrets)
+        if not any(secrets):
+            print("No secrets are found in project "+gcp_project_id+" with filter "+filter_str)
+            sys.exit(1)
+        else:
+            return secrets
+    except Exception as e:
+        print("ERROR : "+str(e))
+        sys.exit(1)
+
+def access_secret_version(gcp_project_id, secrets, client):
+
+    secrets_dict = {}
+    secrets_list = []
+
+    print("\nGetting all the secrets values")
+    for secret in secrets.keys():
+      secrets_list.append(secret)
+      print(secret)
+      
+      # Build the resource name of the secret version.
+      name = f"projects/{gcp_project_id}/secrets/{secret}/versions/latest"
+
+      # Access the secret version
+      try:
+        response = client.access_secret_version(request={"name": name})
+      # print(response)
+      except Exception as e:
+            print("ERROR : "+str(e))
+            sys.exit(1)
+
+      # Store the secret payload
+      secrets_dict[secret] = response.payload.data.decode("UTF-8")
+
+    return secrets_dict
+
+def secrets_to_env(secrets, gcp_project_id, filename):
+
+    print("\nDumping secrets from project "+gcp_project_id+" to "+filename+" file\n")
+    file = open(filename, "w")
+    try:
+        for key, value in secrets.items():
+            file.write(f"{key.upper()}={value}\n")
+    except Exception as e:
+        print("ERROR : "+str(e))
+        sys.exit(1)
+    finally:
+        file.close()
+
+def secrets_to_json(secrets, gcp_project_id, filename):
+
+    print("\nDumping secrets from project "+gcp_project_id+" to "+filename+".json file\n")
+    try:
+        with open(filename+".json", "w") as file:
+            json.dump(secrets, file, indent=2, sort_keys=False)
+    except Exception as e:
+        print("ERROR : "+str(e))
+        sys.exit(1)
+    finally:
+        file.close()
+
+if __name__ == "__main__":
+    main()
